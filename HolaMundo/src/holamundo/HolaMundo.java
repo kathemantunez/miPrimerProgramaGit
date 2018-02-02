3a2bf281d6c1c20505a43cd9dee19a513678d14d
kathe_mantunez <kathe_mantunez@unitec.edu>
2018-02-01 19:39:13 -0600

    hice dos souts

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author KATHERINE
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("cada vez que hacemos un cambio , git add -A, para agregar cambios a github");
+        System.out.println("Despues procedemos hacer git commit -m segido de un mensaje de lo que hiciste");
+    }
+    
+}
